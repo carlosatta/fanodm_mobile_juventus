c51611ad08b1712c2aa591d947dcbb75994002d6
Andrea <andrea.cara@wasabipeas.it>
2013-02-11 12:39:41 +0100

    a

---
+++ js/splashscreen.js
@@ -0,0 +1,21 @@
+var Splashscreen = {
+		
+	do_login: function (){
+		Service.login(
+				'BAABo0L9M21kBAMiccS2vZAWDsXorggEujt6oshQVkwAHZACdvlZAZC0DNc4T6mSbicIjTKyRxua7BhtLRzt4YGkxQGJIXpAOmJYmcoiWYCOg51NZB5ZC5OiZCZCSHqIM4C5a9sKnXVZBNdADMU2eHN253', 
+				'5554321'
+		);
+	},
+	
+	validate_login: function (){
+		if(Service.user_profile.value.id){
+			$.mobile.changePage( "#dashboard", {
+		        transition: "slide",
+		        changeHash: false
+		    }); 
+		}else{
+			alert('C\'è stato un errore durante il login, riprovare.');
+		}
+		
+	}
+};
